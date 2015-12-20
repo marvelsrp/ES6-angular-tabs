aef0f57477a4fcf731a9336717cd8ce3e73e651a
marvelsrp <marvelsrp@gmail.com>
2015-12-20 15:41:01 +0200

    settings karma add 2 basic tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,35 +1,44 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
+    frameworks: ['jasmine', 'browserify'],
     files: [
       'dist/bower_components/angular/angular.min.js',
+      "dist/bower_components/angular-mocks/angular-mocks.js",
       'dist/js/bundle.min.js',
       'dist/js/templates.js',
-      'src/**/.spec.js',
+      'src/**/*.spec.js',
       // fixtures
       {pattern: 'dist/mock/**/*.json', included: false}
     ],
     exclude: [],
     preprocessors: {
-     // 'app/**/**/**/*.json'   : ['json_fixtures'],
-     // 'fixtures/*.json': ['json_fixtures']
+      'src/**/*.spec.js': ['browserify']
     },
-    jsonFixturesPreprocessor: {
-      variableName: '__json__'
+    browserify: {
+      debug: true,
+      transform: ['babelify']
     },
+    //babelPreprocessor: {
+    //  options: {
+    //    presets: ['es2015'],
+    //    sourceMap: 'inline'
+    //  }
+    //},
     proxies: {},
     reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    frameworks: ['jasmine'],
     browsers: [
       'PhantomJS'
     ],
     plugins: [
       'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-browserify',
+      'karma-babel-preprocessor'
     ],
     captureTimeout: 60000,
     singleRun: false
--- src/tabs/tabs.directive.spec.js
+++ src/tabs/tabs.directive.spec.js
@@ -1,14 +1,58 @@
 /*jslint es6: true */
 import {Tabs}       from './tabs.directive.js';
 
-describe('Tabs', function() {
-  let ctrl;
+describe('Tabs', function () {
+  var $compile,
+    $scope,
+    $sce,
+    html =  '<tabs active="active" \
+          on-init="onInit" \
+          on-change-tab="onChangeTab" \
+          on-add="onAdd" \
+          on-remove="onRemove"> \
+          <tab ng-repeat="tab in tabs"> \
+            <tab-header title="tab.title"></tab-header> \
+            <tab-content ng-bind-html="tab.content"></tab-content> \
+          </tab> \
+        </tabs>';
 
-  beforeEach(() => {
-    ctrl = new Tabs();
-  });
+  beforeEach(angular.mock.module("app"));
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$sce_){
 
-  it('should be defined', function () {
-    expect(ctrl).toBeDefined();
-  });
+    $compile = _$compile_;
+    $scope = _$rootScope_.$new();
+    $sce = _$sce_;
+    $scope.tabs = [
+      {
+        title: 'test title 1',
+        content: $sce.trustAsHtml('test content 1')
+      },{
+        title: 'test title 2',
+        content: $sce.trustAsHtml('test content 2')
+      }
+    ];
+    $scope.active = 0;
+    $scope.onChangeTab = function (newTab, oldTab) {
+      console.info('Change tab from ', oldTab.index, ' to ', newTab.index);
+    };
+    $scope.onAdd = function (newTab) {
+      console.info('Add new tab', newTab.index);
+    };
+    $scope.onRemove = function (removeTab) {
+      console.info('Remove tab', removeTab.index);
+    };
+  }));
+  it('To be defined', inject(function() {
+
+    var element = $compile(html)($scope);
+    $scope.$digest();
+    expect(element.html()).toBeDefined();
+  }));
+
+  it('Have tabs bar', inject(function() {
+
+    var element = $compile(html)($scope);
+    $scope.$digest();
+    expect(element.html()).toContain('<span class="text-wrapper ng-binding">test title 1</span>');
+  }));
 });
